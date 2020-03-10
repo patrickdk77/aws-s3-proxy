858ce9bb08e4c493b8dec2e17d217ab66daa3c99
alexander.chernov <alexander.chernov@ticketmaster.co.uk>
2020-03-10 16:33:43 +0000

    renamed health_check prometheus label.

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -7,7 +7,7 @@ import (
 
 var (
 	HealthCheck = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "health_check",
+		Name: "healthcheck_http_requests_total",
 		Help: "Health check response codes",
 	}, []string{"status"})
 )
